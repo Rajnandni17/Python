441e8029673e488b7e2c9456b54db10879e00c7d
Rajnandni17 <rajnandnichandra4155@gmail.com>
2026-08-19 17:58:37 +0530

    make calculater

---
+++ Modules/numpy_ex.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40])
+
+print(numbers)
+print(numbers.sum())
+print(numbers.mean())
+
+
---
+++ Modules/pandas_example.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "Name": ["Raj", "Amit", "Neha"],
+    "Age": [22, 25, 21]
+}
+
+table = pd.DataFrame(data)
+
+print(table)
